3f37c3a813c169ede6b58245380b9736b135a040
Alexey S <leshgans@gmail.com>
2022-11-07 18:02:29 +0300

    vpn native module

---
+++ ios/RCTVpnModule.h
@@ -0,0 +1,14 @@
+//
+//  RCTVpnModule.h
+//  proxyLine
+//
+//  Created by Alex on 07.11.2022.
+//
+
+#ifndef RCTVpnModule_h
+#define RCTVpnModule_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+#endif /* RCTVpnModule_h */
--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,8 +1,41 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, SafeAreaView, ScrollView } from 'react-native'
+import ProxyTariff from '../componets/ProxyTariff'
+import { NativeModules, Button } from 'react-native'
 
-function TestScreen() {
-  return <View />
+const { RNIpSecVpn } = NativeModules
+
+const TestScreen = () => {
+  const onPress = () => {
+    RNIpSecVpn.prepare()
+    RNIpSecVpn.connect('us1.vpnbook.com', 'vpnbook', '88wxtet', 'IPsec', 0)
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Button title="Click to invoke your native module!" color="#841584" onPress={onPress} />
+        <ProxyTariff />
+        <ProxyTariff />
+        <ProxyTariff />
+        <ProxyTariff />
+        <ProxyTariff />
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+  },
+})
 
 export default TestScreen
