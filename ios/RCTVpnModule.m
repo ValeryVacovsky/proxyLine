14b3a6f3d4ee49c271d34e511ae406a7ca4f863f
Alexey S <leshgans@gmail.com>
2022-12-08 12:37:52 +0300

    vpn native module

--- ios/RCTVpnModule.m
+++ ios/RCTVpnModule.m
@@ -9,12 +9,17 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(RNIpSecVpn, RCTEventEmitter)
+@interface RCT_EXTERN_MODULE(VPNManager, NSObject)
 
-RCT_EXTERN_METHOD(prepare:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
-RCT_EXTERN_METHOD(connect:(NSString *)address username:(NSString *)username password:(NSString *)password vpnType:(NSString *)vpnType mtu:(NSNumber *_Nonnull)mtu findEventsWithResolver:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
-RCT_EXTERN_METHOD(disconnect:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
-RCT_EXTERN_METHOD(getCurrentState:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
-RCT_EXTERN_METHOD(getCharonErrorState:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
+//RCT_EXTERN_METHOD(connect:(NSString *)server psk:(NSString *)psk onSuccess:(((Bool) -> Void))onSuccess onError:(((String) -> Void))onError)
+RCT_EXTERN_METHOD(connect)
+
+//@interface RCT_EXTERN_MODULE(RNIpSecVpn, RCTEventEmitter)
+//
+//RCT_EXTERN_METHOD(prepare:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
+//RCT_EXTERN_METHOD(connect:(NSString *)address username:(NSString *)username password:(NSString *)password vpnType:(NSString *)vpnType mtu:(NSNumber *_Nonnull)mtu findEventsWithResolver:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
+//RCT_EXTERN_METHOD(disconnect:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
+//RCT_EXTERN_METHOD(getCurrentState:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
+//RCT_EXTERN_METHOD(getCharonErrorState:(RCTPromiseResolveBlock)findEventsWithResolver rejecter:(RCTPromiseRejectBlock)rejecter)
 
 @end
--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import { StyleSheet, SafeAreaView, ScrollView } from 'react-native'
-import ProxyTariff from '../componets/ProxyTariff'
 import { NativeModules, Button } from 'react-native'
 
-const { RNIpSecVpn } = NativeModules
+const { VPNManager } = NativeModules
 
 const TestScreen = () => {
   const onPress = () => {
-    RNIpSecVpn.prepare()
-    RNIpSecVpn.connect('us1.vpnbook.com', 'vpnbook', '88wxtet', 'IPsec', 0)
+    // RNIpSecVpn.prepare()
+    // RNIpSecVpn.connect()
+    // VPNManager.connect('164.92.138.94', '123ZQvboM7aI+PO6dtHsCgXpnX4WxDK0Uz+ho6mY48fh0g=')
+    VPNManager.connect()
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
         <Button title="Click to invoke your native module!" color="#841584" onPress={onPress} />
-        <ProxyTariff />
-        <ProxyTariff />
-        <ProxyTariff />
-        <ProxyTariff />
-        <ProxyTariff />
       </ScrollView>
     </SafeAreaView>
   )
